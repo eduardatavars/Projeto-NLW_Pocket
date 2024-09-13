e222e063577173ecda520231441c92c9a63cf092
Eduarda Tavares <mariaeduardtavares@gmail.com>
2024-09-13 16:23:58 -0300

    metas realizadas

--- index.js
+++ index.js
@@ -51,6 +51,22 @@ const listarMetas = async () => {
     console.log('Meta(s) marcadas como concluída(s)')
 }
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if(realizadas.length == 0) {
+        console.log('Não existem metas realizadas! :(')
+        return
+    }
+
+    await select({
+        message: "Metas realizadas",
+        choices: [...realizadas]
+    })
+}
+
 const start = async () => {
 
     while(true) {
@@ -67,6 +83,10 @@ const start = async () => {
                     value: "listar"
                 },
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -80,6 +100,9 @@ const start = async () => {
                 break
             case "listar":
                 await listarMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
                 break
             case "sair":
                 console.log("Até a próxima! :)")
